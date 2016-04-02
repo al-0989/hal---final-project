8a18fd6217b3486e0474a7f8fd9fa051d1e8ef37
An La <a.ngoc.la@gmail.com>
2016-04-02 13:59:14 -0700

    Front end cleaned up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 var fs = require('fs');
 var jsdom = require('jsdom');
 var $ = require('jquery');
@@ -24,7 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname + '/public/images/favicon.ico')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,10 +47,6 @@ webSocketServer.on('connection', function(socket){
   socket.on('playVideo', function(data) {
     console.log(data);
   });
-  // socket.on("keyCodePress", function(data){
-  //   console.log("HI>>>>>>>>>>>>>>>>>>");
-  //   console.log(data);
-  // });
 });
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HAL - The home alarm system' });
+  res.render('index');
 });
 
 module.exports = router;
