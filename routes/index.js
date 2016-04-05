d3e6385bb78b87c38b2d654b11017d95f387f7f5
An La <a.ngoc.la@gmail.com>
2016-04-05 11:30:52 -0700

    Camera edit 2

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var raspicam = require('raspicam');
-var cameraOptions = {
-  mode        : "video",
-  output      : 'images/camera.h264'
-};
-
-// Create a new raspicam instance
-var camera = new raspicam(cameraOptions);
-
-camera.start()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  var raspicam = require('raspicam');
+  var cameraOptions = {
+    mode        : "video",
+    output      : 'images/camera.h264'
+  };
+
+  // Create a new raspicam instance
+  var camera = new raspicam(cameraOptions);
+
+  camera.start()
+  // res.render('index');
   res.sendFile(__dirname + '/images/camera.h264');
 });
 
